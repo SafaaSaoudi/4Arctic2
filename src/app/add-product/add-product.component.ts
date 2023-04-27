7748ce4bf491655fa368481b68d438103592624b
SafaaSaoudi <safa.saoudi@esprit.tn>
2023-04-27 11:08:37 +0100

    tdf

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-product',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AddProductComponent {
 
+
+  Save(F: NgForm){
+console.log(F);
+
+  }
 }
